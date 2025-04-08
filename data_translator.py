970657a8bafcdb12b741225451443ce4d1932e81
Alex Carrega <contact@alexcarrega.com>
2025-04-08 12:29:44 +0200

    Automatic commit with GitManager

--- data_translator.py
+++ data_translator.py
@@ -5,9 +5,12 @@ from log import logger
 
 
 def translate(data: Dict, template: Dict) -> str:
-    if "value" in data:
-        template["e"][0]["v"] = data["value"]
-    else:
-        template["value"] = data["e"][0]["v"]
-    logger.info(f"in: {data} out: {template}")
-    return json.dumps(template)
+    try:
+        if "value" in data:
+            template["e"][0]["v"] = data["value"]
+        else:
+            template["value"] = data["e"][0]["v"]
+        logger.info(f"in: {data} out: {template}")
+        return json.dumps(template)
+    except Exception as e:
+        logger.error(e)
