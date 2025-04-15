f7ad7f8486cd53651d63755719dffdfceb650199
Alex Carrega <contact@alexcarrega.com>
2025-04-15 15:09:38 +0200

    Automatic commit with GitManager

--- data_translator.py
+++ data_translator.py
@@ -4,14 +4,26 @@ from typing import Dict
 from log import logger
 
 
+def get_float(s: str) -> float | str:
+    try:
+        return float(s)
+    except ValueError:
+        return s
+
+
 def translate(data: Dict | str, template: Dict) -> str:
     if isinstance(data, str):
         data = json.loads(data)
     try:
         if "value" in template:
-            template["value"] = template["value"].replace("<VALUE>", str(data["Value"]))
+            if "Value" in data:
+                template["value"] = template["value"].replace("<VALUE>", str(data["Value"]))
+            else:
+                template["value"] = template["value"].replace("<VALUE>", str(data["Status"]))
         elif "Value" in template:
-            template["Value"] = float(template["value"].replace("<VALUE>", data["value"]))
+            template["Value"] = get_float(template["value"].replace("<VALUE>", data["value"]))
+        elif "Status" in template:
+            template["Status"] = get_float(template["value"].replace("<VALUE>", data["value"]))
         else:
             logger.warning(f"Unknown payload: {template}")
         logger.info(f"in: {data} out: {template}")
