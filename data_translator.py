1c690cab6738dc0886ec7a190f9c5011ba72c7e3
Alex Carrega <contact@alexcarrega.com>
2025-04-16 13:44:42 +0200

    Automatic commit with GitManager

--- data_translator.py
+++ data_translator.py
@@ -15,6 +15,7 @@ def translate(data: Dict | str, template: Dict) -> str:
     if isinstance(data, str):
         data = json.loads(data)
     try:
+        logger.info(f"Translating from {template} using {data}")
         out = template.copy()
         if "value" in template:
             if "Value" in data:
@@ -22,12 +23,14 @@ def translate(data: Dict | str, template: Dict) -> str:
             else:
                 out["value"] = template["value"].replace("<VALUE>", str(data["Status"]))
         elif "Value" in template:
-                out["Value"] = get_float(template["Value"].replace("<VALUE>", data["value"]))
+            out["Value"] = get_float(template["Value"].replace("<VALUE>", data["value"]))
         elif "Status" in template:
             out["Status"] = get_float(template["Status"].replace("<VALUE>", data["value"]))
         else:
             logger.warning(f"Unknown payload: {template}")
-        logger.info(f"in: {data} out: {out}")
+        logger.info(f"Translated {data} to {out}")
         return json.dumps(out)
+    except KeyError as ke:
+        logger.error("Translate {data} to {out} using {template} [{ke}]")
     except Exception as e:
         logger.error(e)
